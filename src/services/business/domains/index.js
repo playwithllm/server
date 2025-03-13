959e5962d116252e96447808dcd321f4374d7bf0
Foyzul Karim <foyzulkarim@gmail.com>
2025-03-13 17:45:24 +1100

    feat: Remove product routes from route definitions

--- src/services/business/domains/index.js
+++ src/services/business/domains/index.js
@@ -1,11 +1,9 @@
 const inferenceRoutes = require('./inference');
 const apiKeysRoutes = require('./apiKeys');
-const productRoutes = require('./product');
 
 const defineRoutes = async (expressRouter) => {
   inferenceRoutes(expressRouter);
   apiKeysRoutes(expressRouter);
-  await productRoutes(expressRouter);
 };
 
 module.exports = defineRoutes;
